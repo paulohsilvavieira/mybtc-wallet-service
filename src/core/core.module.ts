1d12b3ff11b383c0bc36cae5f19969874ab1fd22
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2023-12-31 17:18:45 -0300

    chore: import usecases

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,9 +1,9 @@
 import { InfraModule } from '@/infra/infra.module';
 import { Module } from '@nestjs/common';
-import { CreateWalletBitcoin } from './domain/protocols/usecases';
-import { CreateWalletBitcoinUseCase } from './application/usecases/create-wallet-usecase';
 import { GetWallet } from './domain/protocols/usecases/get-wallet';
 import { GetWalletUseCase } from './application/usecases';
+import { CreateWalletBitcoin } from './domain/protocols/usecases/bitcoin';
+import { CreateWalletBitcoinUseCase } from './application/usecases/create-wallet-bitcoin-usecase';
 
 @Module({
   imports: [InfraModule],
